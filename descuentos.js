2a522d1055b4cd71061a69569755b4eb5cabc8a5
sebassg7 <sebastlopez@unicauca.edu.co>
2023-04-06 12:05:41 -0500

    Cupones de descuento agregados: if

--- descuentos.js
+++ descuentos.js
@@ -5,6 +5,7 @@ const coupon = document.querySelector('#coupon');
 const button = document.querySelector('#button');
 const total = document.querySelector('.full-record');
 
+
 button.addEventListener('click',calcularDescuento );
 
 function calcularDescuento(){
@@ -16,7 +17,7 @@ function calcularDescuento(){
 
 // DESCUENTO CON CANTIDAD Y DESCUENTO
 
-if(priceInital > 1 && amountInitial > 0 && discountInitial >= 0){
+if(priceInital > 1 && amountInitial >= 1 && discountInitial >= 0){
     
     const priceWithAmount = priceInital * amountInitial;
     const totalWithAmount = priceWithAmount;  
@@ -29,7 +30,7 @@ if(priceInital > 1 && amountInitial > 0 && discountInitial >= 0){
 
 } else if (priceInital > 1 && amountInitial === 0 && discountInitial > 0){ // DESCUENTO SIN CANTIDAD Y DESCUENTO
     
-    const noAmount = 1;
+    const noAmount = 0;
     const priceNoAmount = priceInital * noAmount;
     const totalNoAmount = priceNoAmount;
     
@@ -40,9 +41,9 @@ if(priceInital > 1 && amountInitial > 0 && discountInitial >= 0){
  
     total.innerText = '$ ' + totalNoAmountAndDiscount + ' COP';
     
-} else {// DESCUENTO SIN CANTIDAD Y SIN DESCUENTO
+}  else {// DESCUENTO SIN CANTIDAD Y SIN DESCUENTO
     
-    const noAmount2 = 1;
+    const noAmount2 = 0;
     const priceNoAmount2 = priceInital * noAmount2;
     const totalNoAmount2 = priceNoAmount2;
 
@@ -52,18 +53,38 @@ if(priceInital > 1 && amountInitial > 0 && discountInitial >= 0){
     total.innerText = '$ ' + totalNoAmountAndDiscount2 + ' COP';
 };
 
-// ANUNCIO SOBRE DESCUENTOS
+// CUPONES DE DESCUENTOS
 
 if(discountInitial > 100){
     total.innerText = 'No hay descuentos de más del 100 %'
 };
+
+const couponInitial = coupon.value;
+
+if(!priceInital){
+    total.innerText = "Escribir valores POR FAVOR!!"
+    return;
 };
+
+let discountCoupon;
+
+if(couponInitial == 'Metallica'){
+    discountCoupon = 50;
+
+    const couponMetallica = (priceInital * amountInitial * ((100-discountCoupon)/100)) * ((100-discountInitial)/100);
+    total.innerText = 'El precio con el cupon de descuento es $: ' + couponMetallica;
+
+} else if (couponInitial == 'Megadeth'){
+    discountCoupon = 15;
+
+    const couponMegadeth = priceInital * amountInitial * ((100-discountCoupon)/100) * ((100-discountInitial)/100);
+    total.innerText = 'El precio con el cupon de descuento es $: ' + couponMegadeth;
+
+} else {
+    return;
+};
+};
+
      
-
-
-
-    
-
-
 
     
